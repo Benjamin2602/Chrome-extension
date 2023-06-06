72b3fee0b945d5dad09db0a7b9e2609dc6872b46
Benjamin2602 <91728550+Benjamin2602@users.noreply.github.com>
2023-06-06 18:15:20 +0530

    template string and clearing the input field

--- index.js
+++ index.js
@@ -8,13 +8,20 @@ const ulEl = document.getElementById('ul-el')
 
 inputBtn.addEventListener('click', function() {
 leads.push(inputEl.value)
+//clearing the input field after adding the value to the array
+inputEl.value = ""
 renderLead()
 })
 
 function renderLead(){
     let listItems = ""
     for(let i = 0; i < leads.length; i++){
-        listItems += "<li>" + leads[i] + "</li>"
+        listItems += `
+        <li>
+        <a target='_blank' href='${leads[i]}'>
+        ${leads[i]}
+        </a>
+        </li>`
     }
     ulEl.innerHTML = listItems
 }
