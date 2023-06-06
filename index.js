68b00368dcd5ac079bf0a41fa74437c9cf6a8687
Benjamin2602 <91728550+Benjamin2602@users.noreply.github.com>
2023-06-06 14:23:44 +0530

    rendering the input elements in the screen

--- index.js
+++ index.js
@@ -3,9 +3,18 @@
 const inputEl = document.getElementById('input-el')
 const inputBtn = document.getElementById('input-btn')
 let leads = []
+const ulEl = document.getElementById('ul-el')
 
 
 inputBtn.addEventListener('click', function() {
 leads.push(inputEl.value)
-console.log(leads)
+renderLead()
 })
+
+function renderLead(){
+    let listItems = ""
+    for(let i = 0; i < leads.length; i++){
+        listItems += "<li>" + leads[i] + "</li>"
+    }
+    ulEl.innerHTML = listItems
+}
