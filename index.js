4b8313338da28f7ce578788ed6e8e9b701b2dac6
Benjamin2602 <91728550+Benjamin2602@users.noreply.github.com>
2023-06-10 23:22:35 +0530

    tab button

--- index.js
+++ index.js
@@ -1,51 +1,59 @@
-
-
-let leads = []
-const inputEl = document.getElementById('input-el')
-const inputBtn = document.getElementById('input-btn')
-const ulEl = document.getElementById('ul-el')
-const deleteBtn = document.getElementById('delete-btn')
+let myLeads = [];
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
+const deleteBtn = document.getElementById("delete-btn");
+const tabBtn = document.getElementById("tab-btn");
 
 //to save the array in local storage
-let leadsFromLocalStorage = JSON.parse(localStorage.getItem('Leads'))
-console.log(leadsFromLocalStorage)
+// let leadsFromLocalStorage = JSON.parse(localStorage.getItem('Leads'))
+// console.log(leadsFromLocalStorage)
 
 // if leadFromLocalStorage is truthy (it exists) then set the leads variable to that value
-if(leadsFromLocalStorage){
-    leads = leadsFromLocalStorage
-    renderLead()
+if (leadsFromLocalStorage) {
+  myLeads = leadsFromLocalStorage;
+  renderLead(myLeads);
 }
 
-deleteBtn.addEventListener('dblclick', function(){
-    //clearing the local storage
-    localStorage.clear()
-    //clearing the array
-    leads = []
-    //clearing the dom by rendering the empty array
-    renderLead()
-})
-
-inputBtn.addEventListener('click', function() {
-leads.push(inputEl.value)
-//clearing the input field after adding the value to the array
-inputEl.value = ""
-//to save the item in local storage
-localStorage.setItem('Leads', JSON.stringify(leads))
-renderLead()
-
-//to get the item from local storage
-console.log(localStorage.getItem('Leads'))
-})
-
-function renderLead(){
-    let listItems = ""
-    for(let i = 0; i < leads.length; i++){
-        listItems += `
+function renderLead(leads) {
+  let listItems = "";
+  for (let i = 0; i < leads.length; i++) {
+    listItems += `
         <li>
         <a target='_blank' href='${leads[i]}'>
         ${leads[i]}
         </a>
-        </li>`
-    }
-    ulEl.innerHTML = listItems
+        </li>`;
+  }
+  ulEl.innerHTML = listItems;
 }
+
+deleteBtn.addEventListener("dblclick", function () {
+  //clearing the local storage
+  localStorage.clear();
+  //clearing the array
+  myLeads = [];
+  //clearing the dom by rendering the empty array
+  renderLead(myLeads);
+});
+
+inputBtn.addEventListener("click", function () {
+  myLeads.push(inputEl.value);
+  //clearing the input field after adding the value to the array
+  inputEl.value = "";
+  //to save the item in local storage
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
+  render(myLeads);
+});
+
+const tabs = [
+    {url: "https://www.linkedin.com/in/per-harald-borgen/"}
+]
+
+tabBtn.addEventListener("click", function(){
+    // Save the url instead of logging it out
+    // console.log(tabs[0].url)
+    myLeads.push(tabs[0].url)
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+    render(myLeads)
+})
